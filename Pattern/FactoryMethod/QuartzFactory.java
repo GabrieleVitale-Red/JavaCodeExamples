1baaa69107d93ceded71fa27e95f6c0056ca7130
GabrieleVitaleRed <vitalegbr@gmail.com>
2020-05-13 16:24:22 +0200

    Creation of branch lambda

--- Pattern/FactoryMethod/QuartzFactory.java
+++
@@ -1,7 +0,0 @@
-
-public class QuartzFactory extends MineralFactory{
-	//ConcreteCreator: it creates the actual object 
-	public static Work getMineralToBeWorked(){
-		return new Quarzo();
-	}
-}
--- Pattern/FactoryMethod/Work.java
+++
@@ -1,6 +0,0 @@
-
-public interface Work {
-	//Product: the interface that will be implemented by the objects
-	//that will be created using the factory
-	void cut();
-}
